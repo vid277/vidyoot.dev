cedec4f695bf22e6e5cf24f2b136c414bd284e72
Vidyoot Senthil <vidyoot@icloud.com>
2024-09-16 00:36:34 -0400

    cleanup: setup template and removed extra files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold bg-green-100">Typography</h1>;
+  return <div className="">VIDYOOT.DEV</div>;
 }
 
 export default App;
