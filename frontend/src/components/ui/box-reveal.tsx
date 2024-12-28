f4f4e487dba3e7124b960de4eb9411a902782270
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2024-12-27 22:44:57 -0600

    bugfix: error with react import

--- frontend/src/components/ui/box-reveal.tsx
+++ frontend/src/components/ui/box-reveal.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import React from "react";
 import { useEffect, useRef } from "react";
 import { motion, useAnimation, useInView } from "framer-motion";
 
 interface BoxRevealProps {
-  children: JSX.Element;
+  children: React.ReactElement;
   width?: "fit-content" | "100%";
   boxColor?: string;
   duration?: number;
