7eac63fdb5fc3aeb5fdaa675716721c51f2475bd
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2024-12-27 23:10:41 -0600

    cleanup: added padding to about

--- frontend/src/components/about.tsx
+++ frontend/src/components/about.tsx
@@ -9,11 +9,11 @@ import { Badge, badgeVariants } from "@/components/ui/badge";
 export function About() {
   return (
     <div
-      className="h-min w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center"
+      className="h-min w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center py-10"
       id="about"
-      >
+    >
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <div className="h-max relative w-full p-16 px-36 pb-28 flex flex-col justify-start items-start gap-2 ">
+      <div className="h-max relative w-full p-16 px-36 pb-28 flex flex-col justify-start items-start gap-2">
         <div className="flex flex-row justify-center items-start w-full mt-1">
           <div className="flex flex-col gap-2 flex-1">
             <BoxReveal boxColor={"#5046e6"} duration={0.5}>
