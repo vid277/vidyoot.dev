200933055e8688353f47a35a59e17a57312428a3
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2024-12-27 23:00:55 -0600

    fix: text sizing

--- frontend/src/components/about.tsx
+++ frontend/src/components/about.tsx
@@ -11,14 +11,14 @@ export function About() {
     <div className="h-min w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="h-max relative w-full p-16 px-36 pb-28 flex flex-col justify-start items-start gap-2 ">
-        <div className="flex flex-row justify-between items-start w-full mt-1">
+        <div className="flex flex-row justify-center items-start w-full mt-1">
           <div className="flex flex-col gap-2 flex-1">
             <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-              <h1 className="text-5xl font-oddolini">About me.</h1>
+              <h1 className="text-6xl font-oddolini">About me.</h1>
             </BoxReveal>
             <div className="flex flex-col gap-7">
               <div className="w-5/6">
-                <h2 className="text-base font-hanken">
+                <h2 className="text-xl font-hanken">
                   Hi! I'm Vidyoot Senthil, a software engineer based in Atlanta,
                   Georgia. I'm currently a student at the{" "}
                   <span className="font-bold">
@@ -34,32 +34,32 @@ export function About() {
                 </h2>
               </div>
               <div>
-                <h2 className="text-3xl font-bold font-oddolini text-gray-900 mb-3">
+                <h2 className="text-4xl font-bold font-oddolini text-gray-900 mb-3">
                   Hobbies
                 </h2>{" "}
                 <div className="flex flex-row gap-2 flex-wrap w-[85%]">
-                  <Badge variant="outline" className="text-sm font-hanken">
+                  <Badge variant="outline" className="text-base font-hanken">
                     Lifting
                   </Badge>
-                  <Badge variant="outline" className="text-sm font-hanken">
+                  <Badge variant="outline" className="text-base font-hanken">
                     Designing Game Art
                   </Badge>
-                  <Badge variant="outline" className="text-sm font-hanken">
+                  <Badge variant="outline" className="text-base font-hanken">
                     Animating in Blender
                   </Badge>
-                  <Badge variant="outline" className="text-sm font-hanken">
+                  <Badge variant="outline" className="text-base font-hanken">
                     Learning Premiere Pro
                   </Badge>
-                  <Badge variant="outline" className="text-sm font-hanken">
+                  <Badge variant="outline" className="text-base font-hanken">
                     Playing BTD 6
                   </Badge>
-                  <Badge variant="outline" className="text-sm font-hanken">
+                  <Badge variant="outline" className="text-base font-hanken">
                     Swimming
                   </Badge>
-                  <Badge variant="outline" className="text-sm font-hanken">
+                  <Badge variant="outline" className="text-base font-hanken">
                     Re-reading Harry Potter
                   </Badge>
-                  <Badge variant="outline" className="text-sm font-hanken">
+                  <Badge variant="outline" className="text-base font-hanken">
                     Scrolling Hacker News
                   </Badge>
                 </div>
