af239610c9a6254024088073d10568ae2130ce5b
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2024-12-28 10:07:21 -0600

    feature: added work title

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { About } from "@/components/about";
 import { Work } from "@/components/work";
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 scroll-smooth">
+    <div className="flex flex-col scroll-smooth">
       <Cover />
       <About />
       <Work />
--- frontend/src/components/work.tsx
+++ frontend/src/components/work.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+"use client";
+
+import { cn } from "@/lib/utils";
+import { DotPattern } from "@/components/ui/dot-pattern";
 
 export function Work() {
-  return <div></div>;
+  return (
+    <div className="h-[100vh] w-full  relative flex items-start justify-start py-16 p-16 px-36 pb-28 ">
+      <h1 className="text-6xl font-oddolini">Work Experience</h1>
+    </div>
+  );
 }
