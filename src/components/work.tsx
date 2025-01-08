37a0822b5fa2a2e26a9865080cc4f6c7eb47dd7c
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2025-01-08 14:28:14 -0500

    refactor: enhance work timeline layout and mobile responsiveness
    - Added responsive classes to improve layout on mobile and desktop views in WorkTimeline component.
    - Introduced a new MobileLink component for better link handling on mobile.
    - Updated Timeline component to accept a className prop for styling flexibility.
    - Adjusted padding and margin in Work component for improved spacing.

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -5,9 +5,9 @@ import BoxReveal from "./ui/box-reveal";
 
 export function Work() {
   return (
-    <div className="h-max w-full  relative flex flex-col items-center md:items-start justify-start py-8 p-16 md:px-36 px-14 mb-28 ">
+    <div className="h-max w-full  relative flex flex-col items-center md:items-start justify-start py-8 p-16 md:px-36 px-16 mb-20 ">
       <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-        <h1 className="text-6xl font-oddolini text-center md:text-left">
+        <h1 className="text-6xl md:text-6xl font-oddolini text-center md:text-left">
           Experience
         </h1>
       </BoxReveal>
